cc517ddda4273fd23272056baf32e90d458f4106
zhongchai <z.chai@students.clark.edu>
2019-02-09 11:16:04 -0800

    v1 basic functionalities working, basic css added, 3 possible parameters

--- page1.php
+++ page1.php
@@ -7,11 +7,39 @@
     <!-- Bootstrap CSS (CDN) -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Project 2 - Page 1</title>
+    <title>Welcome | Z Creature Creator</title>
 </head>
 <body>
 
-
+<div id="wrapper">
+    <div class="header">
+        <img src="img/z-chara-logo.png" alt="Z Creature Creator Logo">
+    </div>
+    <div class="main-content-box">
+        <div class="section">
+            <h2>What is this site?</h2>
+            <p>This is an experimental site where users can add and change parts of the page to create their own creature, just by editing the URL of the site! It is similar to a character creator or a dress-up game, and is completely browser-based.</p>
+        </div>
+        <div class="section">
+            <h2>Instructions</h2>
+            <p>Scroll to the bottom of this page for a link to open up the page with the character creator. On this page, you can append certain phrases to the end of the URL to modify different aspects of the creature. Refer to the guide below for a list of parts you can add. To use it, type "?Category=Name" to the end of the URL for the first part you want to edit, then type "&Category=Name" for any subsequent additions.</p>
+            <h2>Guide</h2>
+            <div>
+                <h4>Category: Body</h4>
+                <p>Cat, dog, bug, dragon</p>
+                <h4>Category: Eyes</h4>
+                <p>Open, closed, wink, oof, dot</p>
+                <h4>Category: Mouth</h4>
+                <p>Open, closed, smile, tongue</p>
+                <!-- bg color, name, body, eyes, nose, mouth, wings -->
+            </div>
+            <a href="page2.php" target="_blank"><button type="button" class="btn btn-primary btn-lg">Start!</button></a>
+        </div>
+        <div class="footer">
+            <p>This website is copyright 2019 by Zhong (June) Chai. All graphics on this site are original.</p>
+        </div>
+    </div>
+</div>
 
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
--- page2.php
+++ page2.php
@@ -7,11 +7,102 @@
     <!-- Bootstrap CSS (CDN) -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <title>Project 2 - Page 2</title>
+    <title>Z Creature Creator</title>
 </head>
 <body>
+<div class="frame-container">
+    <div class="main-frame">
+        <img class ="img-bg" src="img/z-base.png">
 
+        <?php 
+            // if 'body=x' is appended to the url, display an image of the matching body
+            if (isset($_GET['body'])){
+                switch ($_GET['body']) {
+                    case 'cat':
+                        echo '<img class ="img-body" src="img/z-body-cat.png">';
+                        break;
+                    
+                    case 'dog':
+                        echo '<img class ="img-body" src="img/z-body-dog.png">';
+                        break;
 
+                    case 'bug':
+                        echo '<img class ="img-body" src="img/z-body-bug.png">';
+                        break;
+
+                    case 'dragon':
+                        echo '<img class ="img-body" src="img/z-body-dragon.png">';
+                        break;
+                    
+                    default: // if url doesn't match the presets, display the default
+                        echo '<img class ="img-body" src="img/z-body-cat.png">';
+                        break;
+                }
+            } else { // if nothing is found in the url, display the default
+                echo '<img class ="img-body" src="img/z-body-cat.png">';
+            }
+        ?>
+
+        <?php 
+            // if 'eyes=x' is appended to the url, display an image of the matching eyes
+            if (isset($_GET['eyes'])){
+                switch ($_GET['eyes']) {
+                    case 'open':
+                        echo '<img class ="img-eyes" src="img/z-eyes-open.png">';
+                        break;
+                    
+                    case 'wink':
+                        echo '<img class ="img-eyes" src="img/z-eyes-wink.png">';
+                        break;
+
+                    case 'angry':
+                        echo '<img class ="img-eyes" src="img/z-eyes-angry.png">';
+                        break;
+                    
+                    case 'oof':
+                        echo '<img class ="img-eyes" src="img/z-eyes-oof.png">';
+                        break;
+                    
+                    default: // if url doesn't match the presets, display the default
+                        echo '<img class ="img-eyes" src="img/z-eyes-dot.png">';
+                        break;
+                }
+            } else { // if nothing is found in the url, display the default
+                echo '<img class ="img-eyes" src="img/z-eyes-dot.png">';
+            }
+        ?>
+
+        <?php 
+            // if 'mouth=x' is appended to the url, display an image of the matching mouth
+            if (isset($_GET['mouth'])){
+                switch ($_GET['mouth']) {
+                    case 'open':
+                        echo '<img class ="img-mouth" src="img/z-mouth-open.png">';
+                        break;
+                    
+                    case 'closed':
+                        echo '<img class ="img-mouth" src="img/z-mouth-closed.png">';
+                        break;
+
+                    case 'smile':
+                        echo '<img class ="img-mouth" src="img/z-mouth-smile.png">';
+                        break;
+
+                    case 'tongue':
+                        echo '<img class ="img-mouth" src="img/z-mouth-tongue.png">';
+                        break;
+                    
+                    default: // if url doesn't match the presets, display the default
+                        echo '<img class ="img-mouth" src="img/z-mouth-closed.png">';
+                        break;
+                }
+            } else { // if nothing is found in the url, display the default
+                echo '<img class ="img-mouth" src="img/z-mouth-closed.png">';
+            }
+        ?>
+
+    </div>
+</div>
 
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
